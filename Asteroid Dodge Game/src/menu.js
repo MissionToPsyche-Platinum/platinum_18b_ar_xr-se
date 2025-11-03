bb5e0c5ee4f74357b972517645fdcd2ddb641c5e
Brayden <brayden.r.brown@comcast.net>
2025-11-03 11:47:38 -0700

    Beginning rewrite of menu

--- Asteroid Dodge Game/src/menu.js
+++ Asteroid Dodge Game/src/menu.js
@@ -51,54 +51,24 @@ export function isMenuVisible() {
     return showMenu;
 }
 
+//Rewrite to use HTML elements instead of draw on the canvas
 export function drawMenuOverlay(ctx) {
     if (!showMenu) return;
+    let menu = document.getElementById("game-menu");
 
-    const W = ctx.canvas.width;
-    const H = ctx.canvas.height;
+    if (!menu) {
+        menu = buildMenuDOM();
+    }
+}
 
-    ctx.save();
-
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.8)';
-    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-    ctx.fillStyle = 'white';
-    ctx.font = '36px Segoe UI';
-    ctx.textAlign = 'center';
-    ctx.fillText('Controls', ctx.canvas.width / 2, 100);
-
-    ctx.fillText("Left", W / 2 - 150, 150);
-    ctx.drawImage(controlLeftArrow, W / 2 -250, 160);
-    ctx.drawImage(controlAKey, W / 2 -150, 160);
-    ctx.fillText("Right", W / 2 + 150, 150);
-    ctx.drawImage(controlRightArrow, W / 2 + 50, 160);
-    ctx.drawImage(controlDKey, W / 2 + 150, 160);
-
-    let mute_icon = isMuted? unmuteIcon : muteIcon;
-    let mute_aspect = mute_icon.width / mute_icon.height;
-
-    const mute_height = 48;
-    const mute_width = mute_height * mute_aspect;
-
-    const mute_x = W - mute_width - MUTE_ICON_PADDING;
-    const mute_y = H - mute_height - MUTE_ICON_PADDING;
-
-    muteButtonBounds = {mute_x, mute_y, mute_width, mute_height};
-
-    const credit_height = 144;
-    const credit_width = 192;
-    const credit_pad = 10;
-    const credit_x = credit_pad;
-    const credit_y = H - credit_pad - credit_height;
-
-    creditsButtonBounds = {credit_height, credit_width, credit_x, credit_y};
-
-    ctx.drawImage(mute_icon, mute_x, mute_y, mute_width, mute_height);
-
-    ctx.fillStyle = 'rgba(106, 106, 106, 0.9)';
-    ctx.fillRect(credit_x, credit_y, credit_width, credit_height);
-
-
-
-    ctx.restore();
+function buildMenuDOM() {
+    const menu = document.createElement("div");
+    menu.id = "game-menu";
+    menu.className = "overlay-menu";
+    menu.innerHTML = `
+    <div class=menuContent>
+    Placeholder
+    </div>
+    `;
+    return menu;
 }
