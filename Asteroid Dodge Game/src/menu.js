bd5a9f65bf9a6c45f4e9f7f5d3064e6559ab2afc
Brayden <brayden.r.brown@comcast.net>
2025-11-08 17:40:51 -0700

    Adding in the controls page

--- Asteroid Dodge Game/src/menu.js
+++ Asteroid Dodge Game/src/menu.js
@@ -51,6 +51,7 @@ export function toggleMenu() {
     } else if (showMenu) {
         const newMenu = buildMenuDOM();
         document.body.appendChild(newMenu);
+        renderMainMenu();
     }
 }
 
@@ -68,36 +69,91 @@ function buildMenuDOM() {
     menu.className = "game-overlay";
     menu.innerHTML = `
     <div class="paused-label">Paused</div>
-    <div id="menu-container" class="menu-container">
-        <div class="menu-buttons">
-            <button id="controls-btn">Controls</button>
-            <button id="credits-btn">Credits</button>
-            <button id="exit-btn">Exit</button>
+    <div id="menu-content"></div>
+    `;
+    return menu;
+}
+
+function renderControlsMenu() {
+    const container = document.createElement("div");
+    container.className = "controls-container";
+    container.innerHTML = `
+    <h2>Controls</h2>
+    <div class="controls-layout">
+        <div class="controls-section">
+            <div class="control-item">
+                <span>Move Left</span>
+                <div class="keys">
+                    <img id="left-key" class="left-key" src="./resources/leftkey.svg" alt="Left Arrow Key" />
+                    <img id="a-key" class="a-key" src="./resources/a_key.svg" alt="A Key" />
+                </div>
+            </div>
+            <div class="control-item">
+                <span>Move Right</span>
+                <div class="keys">
+                    <img id="right-key" class="right-key" src="./resources/rightkey.svg" alt="Right Arrow Key" />
+                    <img id="d-key" class="d-key" src="./resources/d_key.svg" alt="D Key" />
+                </div>
+            </div>
         </div>
-        <div class="mute-container">
-            <img id="mute-icon" src="./resources/mute.svg" alt = "Mute toggle" />
+        <div class="powerups-section">
+            <h3>Powerups</h3>
+            <p>WIP</p>
         </div>
+    </div>
+    <div class="controls-footer">
+        <button id="c-return-btn" class="c-return-btn">Return</button>
     </div>
     `;
 
-    const muteIcon = menu.querySelector("#mute-icon");
+    container.querySelector("#c-return-btn").addEventListener("click", () => {
+        renderMainMenu();
+    });
+
+    updateMenuContent(container);
+}
+
+function renderMainMenu() {
+    const container = document.createElement("div");
+    container.className = "menu-container";
+    container.innerHTML = `
+    <div class="menu-buttons">
+        <button id="controls-btn">Controls</button>
+        <button id="credits-btn">Credits</button>
+        <button id="exit-btn">Exit</button>
+    </div>
+    <div class="mute-container">
+        <img id="mute-icon" src="./resources/mute.svg" alt = "Mute toggle" />
+    </div>
+    `;
+
+    
+    const muteIcon = container.querySelector("#mute-icon");
     muteIcon.addEventListener("click", () => {
         isMuted = !isMuted
         toggleMute();
         muteIcon.src = isMuted ? "./resources/unmute.svg" : "./resources/mute.svg";
     });
 
-    menu.querySelector("#controls-btn").addEventListener("click", () => {
-        alert("open controls menu");
+    container.querySelector("#controls-btn").addEventListener("click", () => {
+        renderControlsMenu();
     });
 
-    menu.querySelector("#credits-btn").addEventListener("click", () => {
+    container.querySelector("#credits-btn").addEventListener("click", () => {
         alert("Open credits page");
     });
 
-    menu.querySelector("#exit-btn").addEventListener("click", () => {
+    container.querySelector("#exit-btn").addEventListener("click", () => {
         alert("Return to hub page");
     });
+
+    updateMenuContent(container);
     
-    return menu;
+}
+
+function updateMenuContent(newContent) {
+    const menuContent = document.getElementById("menu-content");
+    menuContent.innerHTML = '';
+    menuContent.appendChild(newContent);
+    console.log("Updating Menu Content");
 }
